7a006091c92c880b30d82ddad1c2f94a7fc46dd8
Nefta_Camfig <100545486+NEFTACAMFIG@users.noreply.github.com>
2024-03-20 08:32:35 +0100

    Update Exes_and_Ohs.py

--- 7kyu/Exes_and_Ohs.py
+++ 7kyu/Exes_and_Ohs.py
@@ -1,6 +1,7 @@
 #DESCRIPTION:
 
-#Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+#Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. 
+# The string can contain any char.
 
 #Examples input/output:
 
