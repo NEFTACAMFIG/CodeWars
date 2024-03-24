19a65cf8de6e090a805c55805e67772c6b18f7aa
Nefta_Camfig <100545486+NEFTACAMFIG@users.noreply.github.com>
2024-03-24 08:21:38 +0100

    Update and rename 1RM Calculator to 1RM-Calculator.py

--- 6kyu/1RM-Calculator.py
+++ 6kyu/1RM-Calculator.py
@@ -1,15 +1,15 @@
 #DESCRIPTION:
 #You just got done with your set at the gym, and you are wondering how much weight you could lift if you did a single repetition. Thankfully, a few scholars have devised formulas for this purpose (from Wikipedia) :
 
-Epley
-1 RM= w+(1 + r/30)
+# Epley
+# 1 RM= w+(1 + r/30)
  
-McGlothin
-1 RM= 100*w/(101.3-(2.67123*r))
+# McGlothin
+# 1 RM= 100*w/(101.3-(2.67123*r))
 
  
-Lombardi
-1 RM= w*pow(r,0.10)
+# Lombardi
+# 1 RM= w*pow(r,0.10)
  
 #Your function will receive a weight w and a number of repetitions r and must return your projected one repetition maximum. 
 #Since you are not sure which formula to use and you are feeling confident, your function will return the largest value from the three formulas 
