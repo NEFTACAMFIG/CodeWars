367db6d09e951ac75a57ac4e95072e3620633c7c
Nefta_Camfig <100545486+NEFTACAMFIG@users.noreply.github.com>
2024-03-24 08:22:38 +0100

    Update and rename ASCII Fun #1: X- Shape to ASCII-Fun-#1:-X-Shape.py

--- 6kyu/ASCII-Fun-#1:-X-Shape.py
+++ 6kyu/ASCII-Fun-#1:-X-Shape.py
@@ -1,18 +1,18 @@
-DESCRIPTION:
-You will get an odd integer n (>= 3) and your task is to draw an X. Each line is separated with \n.
+# DESCRIPTION:
+# You will get an odd integer n (>= 3) and your task is to draw an X. Each line is separated with \n.
 
-Use the following characters: ■ □ For Ruby, Crystal and PHP: whitespace and o
+# Use the following characters: ■ □ For Ruby, Crystal and PHP: whitespace and o
 
-Examples
+# Examples
 
-                                     ■□□□■
-            ■□■                      □■□■□
-  x(3) =>   □■□             x(5) =>  □□■□□
-            ■□■                      □■□■□
-                                     ■□□□■
+  #                                    ■□□□■
+    #         ■□■                      □■□■□
+  # x(3) =>   □■□             x(5) =>  □□■□□
+    #         ■□■                      □■□■□
+       #                               ■□□□■
 
 
-MY SOLUTION
+# MY SOLUTION
 
 def x(n):
     t = '■' + '□'*(n-2) + '■'
