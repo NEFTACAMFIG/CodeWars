ced4fc7e70b884f2794f6c7cf3706b0c37ec44a4
Nefta_Camfig <100545486+NEFTACAMFIG@users.noreply.github.com>
2024-04-02 09:28:03 +0200

    Update Moving-Zeros-To-The-End.py

--- 5kyu/Moving-Zeros-To-The-End.py
+++ 5kyu/Moving-Zeros-To-The-End.py
@@ -1,10 +1,10 @@
-#DESCRIPTION:
+# DESCRIPTION:
 
-#Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+# Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 
-#move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+# move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
 
-#MY SOLUTION
+# MY SOLUTION
 
 def move_zeros(lst):
     y = []
