dce602dd067635f20abbb2a9ae6920132b6957f2
Nefta_Camfig <100545486+NEFTACAMFIG@users.noreply.github.com>
2024-04-05 08:38:21 +0200

    Update Least-Common-Multiple.py

--- 5kyu/Least-Common-Multiple.py
+++ 5kyu/Least-Common-Multiple.py
@@ -2,7 +2,7 @@
 # Write a function that calculates the least common multiple of its arguments; each argument is assumed to be a non-negative integer. 
 # In the case that there are no arguments (or the provided array in compiled languages is empty), return 1. If any argument is 0, return 0.
 
-#MY SOLUTION
+# MY SOLUTION
 
 import numpy as np
 def lcm(*args):
