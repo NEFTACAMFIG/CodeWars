c6b17b5dbc08ca9367fb4159ae215356cfdeac4c
Nefta_Camfig <100545486+NEFTACAMFIG@users.noreply.github.com>
2024-04-06 06:47:57 +0200

    Update Roman-Numerals-Encoder.py

--- 6kyu/Roman-Numerals-Encoder.py
+++ 6kyu/Roman-Numerals-Encoder.py
@@ -1,29 +1,29 @@
-#DESCRIPTION:
+# DESCRIPTION:
 
-#Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+# Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
 
-#Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+# Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
 
-#Example:
+# Example:
 
-#solution(1000) # should return 'M'
+# solution(1000) # should return 'M'
 
-#Help:
+# Help:
 
-#Symbol    Value
-#I          1
-#V          5
-#X          10
-#L          50
-#C          100
-#D          500
-#M          1,000
+# Symbol    Value
+# I          1
+# V          5
+# X          10
+# L          50
+# C          100
+# D          500
+# M          1,000
 
-#Remember that there can't be more than 3 identical symbols in a row.
+# Remember that there can't be more than 3 identical symbols in a row.
 
-#More about roman numerals - http://en.wikipedia.org/wiki/Roman_numerals
+# More about roman numerals - http://en.wikipedia.org/wiki/Roman_numerals
 
-#MY SOLUTION
+# MY SOLUTION
 
 roman = {
     1:'I', 2:'II', 3:'III', 4:'IV', 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'IX',  
